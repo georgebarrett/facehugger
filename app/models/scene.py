00f29c9d2cf47bd6da973e8fd66d9ae01bad94f4
George Barrett <georgeaustinbarrett@gmail.com>
2023-12-14 10:13:20 +0000

    class LaserWaponArmory constructed. this is responsible for all the logic and text in the laser armory scene

--- app/models/scene.py
+++ app/models/scene.py
@@ -80,3 +80,46 @@ class CentralCorridor(Scene):
         else:
             print('DOES NOT COMPUTE!')
             return 'central_corridor'
+
+class LaserWeaponArmory(Scene):
+    
+    def enter(self):
+        print(('''
+            You do a dive roll into the Weapon Armory, crouch and scan
+            the room for more Gothons that might be hiding. It's
+            quiet, too quiet. You stand up and run to the far side of
+            the room and find the neutron bomb in its container.
+            There's a keypad lock on the box and you need the code to
+            get the bomb out. If you get the code wrong 10 times then
+            the lock closes forever and you can't get the bomb. The
+            code is 3 digits long and the keypad has the numbers 1 to 3.
+        '''))
+
+        code = f'{randint(1,3)}{randint(1,3)}{randint(1,3)}'
+        guess = input('[keypad]> ')
+        guesses = 0
+
+        while guess != code and guesses < 10:
+            print('[BZZZZZEDDDDDD!]')
+            guesses += 1
+            guess = input('[keypad]> ')
+
+        if guess == code:
+            print(('''
+                The container clicks open and the seal breaks, letting
+                gas out. You grab the neutron bomb and run as fast as
+                you can to the bridge where you must place it in the
+                right spot.
+            '''))
+
+            return 'the_bridge'
+
+        else:
+            print(('''
+                The lock buzzes one last time and then you hear a
+                sickening melting sound as the mechanism is fused
+                together. You decide to sit there, and finally the
+                Gothons return to their ship and fire the torpedoes.
+            '''))
+
+            return 'death'
