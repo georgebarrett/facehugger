c45a4e676db2063a5d6d2b0a0fb5eef26792dac8
George Barrett <georgeaustinbarrett@gmail.com>
2023-12-14 10:16:28 +0000

    class EscapePod constructed. this is responsible for all the logic and text in the escape pod scene

--- app/models/scene.py
+++ app/models/scene.py
@@ -169,3 +169,41 @@ class TheBridge(Scene):
         else:
             print('DOES NOT COMPUTE!')
             return 'the_bridge'
+        
+class EscapePod(Scene):
+    
+    def enter(self):
+        print(('''
+            You rush through the ship desperately trying to make it to
+            the escape pod before the whole ship explodes. It seems
+            like hardly any Gothons are on the ship, so your run is
+            clear of interference. You get to the chamber with the
+            escape pods, and now need to pick one to take. Some of
+            them could be damaged but you don't have time to look.
+            There are 5 pods, which one do you take?
+        '''))
+
+        good_pod = randint(1,5)
+        guess = input('[pod#]> ')
+
+        if int(guess) != good_pod:
+            print(f'''
+                You jump into pod {guess} and hit the eject button.
+                The pod escapes out into the void of space, then
+                implodes as the hull ruptures, crushing your body into
+                jam jelly.
+            ''')
+
+            return 'death'
+        
+        else:
+            print((f'''
+                You jump into pod {guess} and hit the eject button.
+                The pod easily slides out into space heading to the
+                planet below. As it flies to the planet, you look
+                back and see your ship implode and then explode like a
+                bright star, taking out the Gothon ship at the same
+                time. You made it out alive!
+            '''))
+
+            return 'finished'
