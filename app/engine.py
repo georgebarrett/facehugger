d8089847b8c52215177b0793d8372b113b4c1ad2
George Barrett <georgeaustinbarrett@gmail.com>
2023-12-14 10:39:34 +0000

    class Map constructed. the class works together with the Engine class and serves as a way to navigate between different scenes in the game

--- app/engine.py
+++ app/engine.py
@@ -13,3 +13,24 @@ class Engine(object):
             current_scene = self.scene_map.next_scene(next_scene_name)
 
         current_scene.enter()
+
+class Map(object):
+
+    scenes = {
+        'central_corridor':CentralCorridor(),
+        'laser_weapon_armory':LaserWeaponArmory(),
+        'the_bridge':TheBridge(),
+        'escape_pod':EscapePod(),
+        'death':Death(),
+        'finished':Finished(),
+    }
+
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
+    def next_scene(self, scene_name):
+        val = Map.scenes.get(scene_name)
+        return val
+
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
