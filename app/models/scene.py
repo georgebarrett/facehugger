948f45d13af5d572715fc30708702ea4944c4736
George Barrett <georgeaustinbarrett@gmail.com>
2023-12-14 11:39:19 +0000

    class EscapePod refactored so that it is suitable for a flask web application

--- app/models/scene.py
+++ app/models/scene.py
@@ -120,41 +120,27 @@ class TheBridge(Scene):
         
 class EscapePod(Scene):
     
-    def enter(self):
-        print(('''
-            You rush through the ship desperately trying to make it to
-            the escape pod before the whole ship explodes. It seems
-            like hardly any Gothons are on the ship, so your run is
-            clear of interference. You get to the chamber with the
-            escape pods, and now need to pick one to take. Some of
-            them could be damaged but you don't have time to look.
-            There are 5 pods, which one do you take?
-        '''))
-
-        good_pod = randint(1,5)
-        guess = input('[pod#]> ')
+    def enter(self, guess=None):
+        good_pod = randint(1, 5)
+        
+        if guess is None:
+            return {
+                "scene": "escape_pod",
+                "message": "You rush through the ship desperately trying to make it to the escape pod before the whole ship explodes. It seems like hardly any Gothons are on the ship, so your run is clear of interference. You get to the chamber with the escape pods, and now need to pick one to take. Some of them could be damaged but you don't have time to look. There are 5 pods, which one do you take?",
+                "pod_numbers": range(1, 6)  # Generate pod numbers 1 to 5
+            }
 
         if int(guess) != good_pod:
-            print(f'''
-                You jump into pod {guess} and hit the eject button.
-                The pod escapes out into the void of space, then
-                implodes as the hull ruptures, crushing your body into
-                jam jelly.
-            ''')
-
-            return 'death'
+            return {
+                "scene": "death",
+                "message": f"You jump into pod {guess} and hit the eject button. The pod escapes out into the void of space, then implodes as the hull ruptures, crushing your body into jam jelly."
+            }
         
         else:
-            print((f'''
-                You jump into pod {guess} and hit the eject button.
-                The pod easily slides out into space heading to the
-                planet below. As it flies to the planet, you look
-                back and see your ship implode and then explode like a
-                bright star, taking out the Gothon ship at the same
-                time. You made it out alive!
-            '''))
-
-            return 'finished'
+            return {
+                "scene": "finished",
+                "message": f"You jump into pod {guess} and hit the eject button. The pod easily slides out into space heading to the planet below. As it flies to the planet, you look back and see your ship implode and then explode like a bright star, taking out the Gothon ship at the same time. You made it out alive!"
+            }
         
 class Finished(Scene):
     
