5d1ee234da6decee7fafc48073266733de614aaa
George Barrett <georgeaustinbarrett@gmail.com>
2023-12-15 15:29:37 +0000

    routes modified to include the 'session' module

--- app/routes.py
+++ app/routes.py
@@ -1,39 +1,40 @@
-from flask import request, render_template, redirect, url_for
-from .models.scene import Death, CentralCorridor, LaserWeaponArmory, TheBridge, EscapePod, Finished
+from flask import Flask, request, render_template, redirect, url_for, session
+from .models.scene import CentralCorridor, LaserWeaponArmory, TheBridge, EscapePod, Finished
 
 def setup_routes(app):
+    @app.route('/')
+    def home():
+        return redirect(url_for('central_corridor'))
+
     @app.route('/central_corridor', methods=['GET', 'POST'])
     def central_corridor():
-        central_corridor_scene = CentralCorridor()
-
+        scene = CentralCorridor()
         if request.method == 'POST':
             action = request.form.get('action')
-            result = central_corridor_scene.enter(action)
+            result = scene.enter(action)
             if result['scene'] == 'death':
-                return redirect(url_for('death', message=result['message']))
+                session['message'] = result['message']
+                return redirect(url_for('death'))
             elif result['scene'] == 'laser_weapon_armory':
                 return redirect(url_for('laser_weapon_armory'))
         else:
-            result = central_corridor_scene.enter()
-        
+            result = scene.enter()
         return render_template('central_corridor.html', result=result)
-
 
     @app.route('/laser_weapon_armory', methods=['GET', 'POST'])
     def laser_weapon_armory():
         armory = LaserWeaponArmory()
-
         if request.method == 'POST':
             guess = request.form.get('guess')
             result = armory.enter(guess)
+            if result['scene'] == 'death':
+                session['message'] = result['message']
+                return redirect(url_for('death'))
+            elif result['scene'] == 'the_bridge':
+                return redirect(url_for('the_bridge'))
         else:
             result = armory.enter()
-        
-        if result['scene'] != 'laser_weapon_armory':
-            return redirect(url_for(result['scene']))
-        
         return render_template('laser_weapon_armory.html', result=result)
-
 
     @app.route('/the_bridge', methods=['GET', 'POST'])
     def the_bridge():
@@ -42,13 +43,12 @@ def setup_routes(app):
             action = request.form.get('action')
             result = bridge.enter(action)
             if result['scene'] == 'death':
-                return redirect(url_for('death', message=result['message']))
-            elif result["scene"] != "the_bridge":
-                return redirect(url_for(result["scene"]))
-        
+                session['message'] = result['message']
+                return redirect(url_for('death'))
+            elif result['scene'] == 'escape_pod':
+                return redirect(url_for('escape_pod'))
         else:
             result = bridge.enter()
-
         return render_template('the_bridge.html', result=result)
 
     @app.route('/escape_pod', methods=['GET', 'POST'])
@@ -57,21 +57,22 @@ def setup_routes(app):
         if request.method == 'POST':
             guess = request.form.get('pod_number')
             result = pod.enter(guess)
+            if result['scene'] == 'finished':
+                session['message'] = result['message']
+                return redirect(url_for('finished'))
+            elif result['scene'] == 'death':
+                session['message'] = result['message']
+                return redirect(url_for('death'))
         else:
             result = pod.enter()
-
-        if result["scene"] != "escape_pod":
-            return redirect(url_for(result["scene"]))
-
         return render_template('escape_pod.html', result=result)
 
     @app.route('/finished')
     def finished():
-        finish_scene = Finished()
-        result = finish_scene.enter()
-        return render_template('finished.html', result=result)
-    
+        message = session.pop('message', 'Congratulations, you made it!')
+        return render_template('finished.html', message=message)
+
     @app.route('/death')
     def death():
-        message = request.args.get('message', 'You have died.')
+        message = session.pop('message', 'You have died.')
         return render_template('death.html', message=message)
